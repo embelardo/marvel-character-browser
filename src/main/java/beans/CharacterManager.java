c7a1c519cf085f684811bb8f5327e5c8bcac3c84
miko belardo <miko belardo>
2017-03-01 22:50:46 -0400

    converted properties file handling into @PropertySource

--- src/main/java/beans/CharacterManager.java
+++ src/main/java/beans/CharacterManager.java
@@ -6,14 +6,13 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import data.MarvelCharacter;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Calendar;
-import java.util.Properties;
 import java.util.logging.Logger;
 
 @SuppressWarnings("unused")
@@ -23,15 +22,8 @@ public class CharacterManager {
     private static final String URL_GET_CHARACTER = "http://gateway.marvel.com/v1/public/characters/{character}?ts={ts}&apikey={apikey}&hash={hash}";
     private Logger logger = Logger.getLogger(CharacterManager.class.getName());
     private RestTemplate restTemplate = new RestTemplate();
-    private Properties apiKeys;
-    private String publicKey;
-    private String privateKey;
-
-    public CharacterManager() throws IOException {
-        apiKeys = initProps();
-        publicKey = apiKeys.getProperty("publicKey");
-        privateKey = apiKeys.getProperty("privateKey");
-    }
+    @Value("${publicKey}") private String publicKey;
+    @Value("${privateKey}") private String privateKey;
 
     public String findCharacter(String characterString) throws UnsupportedEncodingException, NoSuchAlgorithmException {
         logger.info(String.format("find character: '%s'", characterString));
@@ -54,11 +46,5 @@ public class CharacterManager {
     private String getHash(long timeStamp) throws NoSuchAlgorithmException, UnsupportedEncodingException {
         String stringToHash = timeStamp + privateKey + publicKey;
         return DigestUtils.md5Hex(stringToHash);
-    }
-
-    private Properties initProps() throws IOException {
-        Properties p = new Properties();
-        p.load(getClass().getClassLoader().getResourceAsStream("config/apikeys.properties"));
-        return p;
     }
 }
--- src/main/java/config/MvcConfig.java
+++ src/main/java/config/MvcConfig.java
@@ -1,11 +1,20 @@
 package config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@ComponentScan(basePackages = {"beans", "web"})
 @EnableWebMvc
-public class MvcConfig extends WebMvcConfigurerAdapter {}
+@ComponentScan(basePackages = {"beans", "web"})
+@PropertySource("classpath:/config/apikeys.properties")
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
